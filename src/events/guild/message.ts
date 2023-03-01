87013bf479daabd49eacbfdd5d71f029d86a2de4
Jo Project <jo.project.0911@gmail.com>
2023-03-02 06:31:58 +0700

    Update message.ts

--- src/events/guild/message.ts
+++ src/events/guild/message.ts
@@ -2,13 +2,29 @@ import { Message } from "discord.js";
 import { EventBuilder } from "discordbuilder.js";
 import { Bot } from "../../structures/bot.js";
 
+const regex = /\[`(.*)`\]\((.*)\) (.*) - (.*)/;
+
 const message = new EventBuilder(false)
 .setName('messageCreate')
 .setCallback(
     async(client: Bot, message: Message) => {
         if (message.channelId === `1080544269011529808`) {
             console.log(message.content)
-            console.log(message.embeds)
+            const postEmbed = message.embeds[0]
+            const str = postEmbed.description!
+            const matches = str.match(regex);
+
+            if (matches) {
+                const id = matches[1];
+                const link = matches[2];
+                const message = matches[3];
+                const author = matches[4];
+
+                console.log(`ID: ${id}`);
+                console.log(`Link: ${link}`);
+                console.log(`Message: ${message}`);
+                console.log(`Author: ${author}`);
+            }
         }
     }
 )
