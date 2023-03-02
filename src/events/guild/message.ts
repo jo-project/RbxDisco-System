b1c405c330e69ef8e86a0f375333abfd5652ad45
Jo Project <jo.project.0911@gmail.com>
2023-03-02 07:09:17 +0700

    Update message.ts

--- src/events/guild/message.ts
+++ src/events/guild/message.ts
@@ -1,8 +1,10 @@
-import { Message } from "discord.js";
+import { Message, EmbedBuilder, WebhookClient, TextChannel } from "discord.js";
 import { EventBuilder } from "discordbuilder.js";
 import { Bot } from "../../structures/bot.js";
 
+
 const regex = /\[`(.*)`\]\((.*)\) (.*) - (.*)/;
+const another_regex = /^\[(.*):(.*)\] (.*)$/;
 
 const message = new EventBuilder(false)
 .setName('messageCreate')
@@ -11,19 +13,62 @@ const message = new EventBuilder(false)
         if (message.channelId === `1080544269011529808`) {
             console.log(message.content)
             const postEmbed = message.embeds[0]
-            const str = postEmbed.description!
+            const str = postEmbed.description!;
+            const titleStr = postEmbed.title!;
+
             const matches = str.match(regex);
+            const matches_1 = titleStr.match(another_regex)
 
-            if (matches) {
-                const id = matches[1];
-                const link = matches[2];
-                const message = matches[3];
-                const author = matches[4];
+            if (matches_1) {
+                const repoName = matches_1[1];
+                const branch = matches_1[2];
+                const type = matches_1[3];
 
-                console.log(`ID: ${id}`);
-                console.log(`Link: ${link}`);
-                console.log(`Message: ${message}`);
-                console.log(`Author: ${author.replace('\\', '-')}`);
+                if (matches) {
+                    const id = matches[1];
+                    const link = matches[2];
+                    const msg = matches[3];
+                    const author = matches[4];
+
+                    const webhook = new WebhookClient({ id: `1080636459247218698`, token: `h8xoRBD1nIXu7zPYkTz2AReocE6wVnUoeI6SwBjcev0TiKRx7-NHO1Cre9WJDvDCSA7T`})
+
+                    const embedBuilder = new EmbedBuilder()
+                    .setAuthor({
+                        name: postEmbed.author!.name,
+                        iconURL: postEmbed.author!.iconURL || ''
+                    })
+                    .setTitle(`${repoName} on branch ${branch}`)
+                    .setURL(postEmbed.url)
+                    .setColor(postEmbed.color)
+                    .setDescription(`**${type}**`)
+                    .addFields(
+                        {
+                            name: 'Committer',
+                            value: `${author.replace('\\', '')}`
+                        },
+                        {
+                            name: 'ID',
+                            value: id
+                        },
+                        {
+                            name: 'Link',
+                            value: link
+                        },
+                        {
+                            name: 'Message',
+                            value: msg
+                        }
+                    )
+                    .setTimestamp()
+                    .setFooter({
+                        text: `${message.guild!.name} - GitHub`,
+                        iconURL: message.guild!.iconURL({ extension: 'png' }) || ''
+                    })
+
+                    await webhook.send({
+                        embeds: [embedBuilder]
+                    })
+                }
             }
         }
     }
