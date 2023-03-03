efa8ed979aebf0d7bd89d0d6153831c7dd895ff3
Jo Project <jo.project.0911@gmail.com>
2023-03-03 11:40:54 +0700

    Update bot info command

--- src/commands/information/bot.ts
+++ src/commands/information/bot.ts
@@ -7,6 +7,8 @@ import { checkSystem } from "../../extensions/check.js";
 import { stripIndents } from 'common-tags';
 import { snowflakeToDate, dateToUnix } from "../../extensions/convert.js";
 import { checkVersion } from "../../extensions/check.js";
+import mongoose from "mongoose";
+import { logs } from "../../config/bot.config.js";
 
 const bot = new CommandBuilder()
 .setName('bot')
@@ -19,6 +21,16 @@ const bot = new CommandBuilder()
 .addSubcommand(opt => opt
     .setName('info')
     .setDescription('Get information about the bot')
+    .setLevel(0)
+)
+.addSubcommand(opt => opt
+    .setName('ping')
+    .setDescription('Get ping of the bot')
+    .setLevel(0)
+)
+.addSubcommand(opt => opt
+    .setName('changelogs')
+    .setDescription('View changelogs of the bot')
     .setLevel(0)
 )
 .setCallback(
@@ -52,12 +64,12 @@ const bot = new CommandBuilder()
                 name: bot.user!.username,
                 iconURL: bot.user!.displayAvatarURL({ extension: 'png' })
             })
-            .setTitle(`What is ${bot.user!.username}?`)
+            .setTitle(`<:bot:1081005591163572305> <:arrow:1080943378713284648> What is ${bot.user!.username}?`)
             .setDescription(stripIndents`**__About ${bot.user!.username}__**
             >>> <@${bot.user!.id}> is a multipurpose bot to help you run the **__server__**!
-            With more than 50 commands, we have a functional bot with many features to improve your server!`)
+            With **50+** commands, we have a functional bot with many features to improve your server!`)
             .setThumbnail(bot.user!.displayAvatarURL({ extension: 'png' }))
-            .setColor(0x36393F)
+            .setColor(0x65c4c0)
             .addFields(
                 {
                     name: '<:person:1080942557695049748> <:arrow:1080943378713284648> Name',
@@ -70,67 +82,67 @@ const bot = new CommandBuilder()
                     inline: true
                 },
                 {
-                    name: "Shards",
+                    name: "<:shards:1081004296159625216> <:arrow:1080943378713284648> Shards",
                     value: `> \`${bot.shardCount}\` ${bot.shardCount > 1 ? 'shards' : 'shard'}`,
                     inline: true
                 },
                 {
-                    name: 'Owner',
+                    name: '<:owner:1080996570029379684> <:arrow:1080943378713284648> Owner',
                     value: `> <@${process.env.OWNER_ID!}>`,
                     inline: true
                 },
                 {
-                    name: 'Developer',
+                    name: '<:developer:1080996566279667752> <:arrow:1080943378713284648> Developer',
                     value: `> <@${process.env.OWNER_ID!}>`,
                     inline: true
                 },
                 {
-                    name: 'Commands',
+                    name: '<:commands:1080994586803056750> <:arrow:1080943378713284648> Commands',
                     value: `> \`${bot.commands.length}\` commands`,
                     inline: true
                 },
                 {
-                    name: 'Servers',
+                    name: '<:server:1080995483104850020> <:arrow:1080943378713284648> Servers',
                     value: `> \`${guildSize}\` ${guildSize > 1 ? 'servers' : 'server'}`,
                     inline: true
                 },
                 {
-                    name: 'Members',
+                    name: '<:members:1080998944697495582> <:arrow:1080943378713284648> Members',
                     value: `> \`${memberCount}\` ${memberCount > 1 ? 'members' : 'member'}`,
                     inline: true
                 },
                 {
-                    name: 'Created at',
+                    name: '<:calendar_time:1080999524530667600> <:arrow:1080943378713284648> Created at',
                     value: `> <t:${discordAge}>`,
                     inline: true
                 },
                 {
-                    name: 'Uptime',
+                    name: '<:uptime:1080998941287530636> <:arrow:1080943378713284648> Uptime',
                     value: `> ${durationString}`,
                     inline: true
                 },
                 {
-                    name: 'API Speed',
+                    name: '<:api:1080994328811417631> <:arrow:1080943378713284648> API Speed',
                     value: `> \`${bot.ws.ping}\` ms`,
                     inline: true
                 },
                 {
-                    name: 'Version',
+                    name: '<:version:1081004797433491456> <:arrow:1080943378713284648> Version',
                     value: `> \`${version.bot}\``,
                     inline: true
                 },
                 {
-                    name: 'Language',
+                    name: '<:coding:1080993657961840650> <:arrow:1080943378713284648> Language',
                     value: `> <:typescript:1080939281218031706>`,
                     inline: true
                 },
                 {
-                    name: 'Node.js Version',
+                    name: '<:nodejs:1080992170212859926> <:arrow:1080943378713284648> Node.js Version',
                     value: `> \`${version.node}\``,
                     inline: true
                 },
                 {
-                    name: 'Framework Version',
+                    name: '<:discordjs:1080992792903438427> <:arrow:1080943378713284648> Discord.js Version',
                     value: `> \`${version.framework}\``,
                     inline: true
                 }
@@ -138,11 +150,12 @@ const bot = new CommandBuilder()
 
             const systemEmbed = new EmbedBuilder()
             .setAuthor({
-                name: `System Stats`
+                name: `Stats`,
+                iconURL: `https://cdn.discordapp.com/attachments/1072938695956643920/1081002109157257317/computers.png`
             })
             .setDescription(stripIndents`\`\`\`json
             ${systemInfo}\`\`\``)
-            .setColor(0x36393F)
+            .setColor(0x65c4c0)
             .setTimestamp()
             .setFooter({
                 text: `\u00a9 ${bot.user!.username} 2023`,
@@ -150,6 +163,72 @@ const bot = new CommandBuilder()
             })
 
             return interaction.editReply({ embeds: [botEmbed, systemEmbed]})
+        } else if (sub === 'ping') {
+            const reply = await interaction.fetchReply();
+            const db = mongoose.connection;
+            let dbSpeed = 0;
+
+            const resDb = await mongoose.connection.db.admin().ping()
+            dbSpeed = resDb.ok
+
+            const ping = reply.createdTimestamp - interaction.createdTimestamp;
+            const botEmbed = new EmbedBuilder()
+            .setAuthor({
+                name: bot.user!.username,
+                iconURL: bot.user!.displayAvatarURL({ extension: 'png' })
+            })
+            .setTitle(`<:network:1081063340966690886> <:arrow:1080943378713284648> ${bot.user!.username} Ping`)
+            .setColor(0x65c4c0)
+            .addFields(
+                {
+                    name: '<:list:1081063893037760612> <:arrow:1080943378713284648> Bot Latency',
+                    value: `\`\`\`+ ${ping}ms (${ping/1000}s)\`\`\``,
+                    inline: true
+                },
+                {
+                    name: '<:list:1081063893037760612> <:arrow:1080943378713284648> API Latency',
+                    value: `\`\`\`+ ${Math.round(bot.ws.ping)}ms (${bot.ws.ping / 1000}s)\`\`\``,
+                    inline: true
+                },
+                {
+                    name: '<:list:1081063893037760612> <:arrow:1080943378713284648> Database Latency',
+                    value: `\`\`\`+ ${dbSpeed}ms (${dbSpeed / 1000}s)\`\`\``,
+                    inline: true
+                }
+            )
+            .setTimestamp()
+            .setFooter({
+                text: `\u00a9 ${bot.user!.username} 2023`,
+                iconURL: bot.user!.displayAvatarURL({ extension: 'png' })
+            })
+
+            return interaction.editReply({ embeds: [botEmbed] })
+        } else if (sub === 'changelogs') {
+            const botEmbed = new EmbedBuilder()
+            .setAuthor({
+                name: bot.user!.username,
+                iconURL: bot.user!.displayAvatarURL({ extension: 'png' })
+            })
+            .setTitle(`<:log:1081073311414497321> <:arrow:1080943378713284648> Changelogs`)
+            .setDescription(`>>> This is the changelogs of **${bot.user!.username}**, here you can see the changelogs that have been made!`)
+            .setColor(0x65c4c0)
+            .setThumbnail(bot.user!.displayAvatarURL({ extension: 'png'}))
+            .addFields(
+                logs.map(data => {
+                    return {
+                        name: data.date,
+                        value: `>>> ${data.message}`,
+                        inline: true
+                    }
+                })
+            )
+            .setTimestamp()
+            .setFooter({
+                text: `\u00a9 ${bot.user!.username} 2023`,
+                iconURL: bot.user!.displayAvatarURL({ extension: 'png' })
+            })
+
+            return interaction.editReply({ embeds: [botEmbed] })
         }
     }
 )
--- src/config/bot.config.ts
+++ src/config/bot.config.ts
@@ -1,1 +1,8 @@
 export const version = '1.0.0'
+
+export const logs = [
+    {
+        date: '3/03/2023',
+        message: 'Updated bot information command'
+    }
+]
