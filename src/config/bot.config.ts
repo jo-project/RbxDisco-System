3ce0b26d3be5bff9694299b34b7c2591efffefa4
Jo Project <jo.project.0911@gmail.com>
2023-03-04 10:43:29 +0700

    Update bot.config.ts

--- src/config/bot.config.ts
+++ src/config/bot.config.ts
@@ -4,5 +4,9 @@ export const logs = [
     {
         date: '3/03/2023',
         message: 'Updated bot information command'
+    },
+    {
+        date: '4/03/2023',
+        message: 'Updated bot ready system'
     }
 ]
